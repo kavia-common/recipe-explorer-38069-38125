adba1883fc4ef9f5795c137c6e49fd36bbab4866
Kavia AI <production@kavia.ai>
2025-11-03 20:19:02 +0000

    Checkpoint 48 - cgf6645f90

--- frontend_react_js/src/index.js
+++ frontend_react_js/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+/**
+ * PUBLIC_INTERFACE
+ * React entrypoint: mounts App onto the #root element in public/index.html
+ */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
